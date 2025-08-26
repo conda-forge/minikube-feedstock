abd7b72a5d190314d6f3ff9178f996b9cbcc4acf
Daniel Elsner <daniel.elsner@quantco.com>
2025-08-26 19:36:58 +0200

    Fix go-licenses

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,4 +11,4 @@ mkdir -p $PREFIX/bin
 
 mv out/minikube $PREFIX/bin
 
-go-licenses save $SRC_DIR --save_path="./license-files/"
+go-licenses save $SRC_DIR/cmd/minikube --save_path="./license-files/"
